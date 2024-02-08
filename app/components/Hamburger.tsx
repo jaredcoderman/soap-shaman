4c207c33de99298b64c58c09ecb768305e7ffeda
Jared Head <jaredheadd@gmail.com>
2024-02-08 14:55:15 -0500

    dropdown base finished

---
+++ app/components/Hamburger.tsx
@@ -0,0 +1,55 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Session } from "next-auth";
+
+export type Props = {
+  session: Session | null;
+};
+
+const Hamburger: React.FC<Props> = (props) => {
+  const { session } = props;
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <div
+        className="flex lg:hidden"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        <Image
+          alt="hamburger button icon"
+          src="/hamburger.png"
+          width={40}
+          height={40}
+        ></Image>
+      </div>
+      {open && (
+        <div className="absolute pr-12 z-20 right-5 bg-white rounded border-1 border-black ">
+          <Link href="/about">
+            <p className="text-left py-1 text-lg mx-4">About</p>
+          </Link>
+          <Link href="/blog">
+            <p className="text-lg pb-1  mx-4">Blog</p>
+          </Link>
+          <Link href="/">
+            <p className="text-lg pb-1 mx-4">Home</p>
+          </Link>
+          {session ? (
+            <Link href="/api/auth/signout">
+              <p className="text-lg ml-4 pb-1 font-bold">Log out</p>
+            </Link>
+          ) : (
+            <Link href="/api/auth/signin">
+              <p className="text-lg ml-4 pb-1 font-bold">Log in</p>
+            </Link>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Hamburger;
