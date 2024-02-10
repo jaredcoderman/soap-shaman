321241abdbb56ae54580cd6dccb0c820360d8ad4
Jared Head <jaredheadd@gmail.com>
2024-02-10 11:52:08 -0500

    products responsive

--- app/components/ProductTile.tsx
+++ app/components/ProductTile.tsx
@@ -11,7 +11,7 @@ const ProductTile: React.FC<Props> = (props) => {
   const { path, alt } = props;
 
   return (
-    <div className="pb-20 mx-auto col-span-2">
+    <div className="pb-20 col-span-4 md:col-span-2">
       <Image src={path} width="250" height="250" alt={alt}></Image>
       <p className="mt-4 text-lg font-medium">{alt}</p>
       <p className="mt-2">$29.99</p>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,7 +5,7 @@ import ProductTile from "../components/ProductTile";
 const page = () => {
   return (
     <div className="mx-8 w-3/5 mx-auto">
-      <h1 className="text-5xl text-center mb-12">Our Selection</h1>
+      <h1 className="text-5xl mb-12">Our Selection</h1>
       <div className="grid grid-cols-4">
         <ProductTile path={"/yellow-bow.jpeg"} alt="Lemon yellow" />
         <ProductTile path={"/aqua.jpg"} alt="Hydrating aqua" />
